72deab3654b2cec7c6e5524da44a96879c9178a2
charlyoleg <charles.braquet@gmx.fr>
2024-11-22 22:19:55 +0100

    prettier for nW

--- pkg/nW/src/index.ts
+++ pkg/nW/src/index.ts
@@ -1,34 +1,38 @@
-import { serve } from '@hono/node-server';
-import { Hono } from 'hono';
-import { z } from 'zod';
-import { zValidator } from '@hono/zod-validator';
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 
 const app = new Hono();
 
 const route = app
-	.get('/', (c) => {
-		return c.text('Hello Hono!');
+	.get("/", (c) => {
+		return c.text("Hello Hono!");
 	})
-	.get('/what',
-		 //zValidator('query', z.object({ name: z.string().optional() }).optional()),
-		 zValidator('query', z.object({ name: z.string() })),
-		 (c) => {
-		const query = c.req.valid('query');
-		//console.log(query);
-		//const name = query.name ? query.name : "blouf";
-		return c.json({
-				ok: true,
-				msg: `What ${query.name}`
-			},
-			200);
-	});
+	.get(
+		"/what",
+		//zValidator('query', z.object({ name: z.string().optional() }).optional()),
+		zValidator("query", z.object({ name: z.string() })),
+		(c) => {
+			const query = c.req.valid("query");
+			//console.log(query);
+			//const name = query.name ? query.name : "blouf";
+			return c.json(
+				{
+					ok: true,
+					msg: `What ${query.name}`,
+				},
+				200,
+			);
+		},
+	);
 
 const port = 3010;
 console.log(`Server is running on http://localhost:${port}`);
 
 serve({
-  fetch: app.fetch,
-  port
+	fetch: app.fetch,
+	port,
 });
 
 export type AppType = typeof route;
