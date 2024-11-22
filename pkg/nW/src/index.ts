fc30391ff22f0b111cffd61bc5f94a5bf9b5dffd
charlyoleg <charles.braquet@gmx.fr>
2024-11-22 22:25:28 +0100

    prettier completed

--- pkg/nW/src/index.ts
+++ pkg/nW/src/index.ts
@@ -1,30 +1,30 @@
-import { serve } from "@hono/node-server";
-import { Hono } from "hono";
-import { z } from "zod";
-import { zValidator } from "@hono/zod-validator";
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import { z } from 'zod';
+import { zValidator } from '@hono/zod-validator';
 
 const app = new Hono();
 
 const route = app
-	.get("/", (c) => {
-		return c.text("Hello Hono!");
+	.get('/', (c) => {
+		return c.text('Hello Hono!');
 	})
 	.get(
-		"/what",
+		'/what',
 		//zValidator('query', z.object({ name: z.string().optional() }).optional()),
-		zValidator("query", z.object({ name: z.string() })),
+		zValidator('query', z.object({ name: z.string() })),
 		(c) => {
-			const query = c.req.valid("query");
+			const query = c.req.valid('query');
 			//console.log(query);
 			//const name = query.name ? query.name : "blouf";
 			return c.json(
 				{
 					ok: true,
-					msg: `What ${query.name}`,
+					msg: `What ${query.name}`
 				},
-				200,
+				200
 			);
-		},
+		}
 	);
 
 const port = 3010;
@@ -32,7 +32,7 @@ console.log(`Server is running on http://localhost:${port}`);
 
 serve({
 	fetch: app.fetch,
-	port,
+	port
 });
 
 export type AppType = typeof route;
