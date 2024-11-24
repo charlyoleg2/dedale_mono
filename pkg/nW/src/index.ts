606efefbe6d093725a3b55c5b38ea920cf9d5359
charlyoleg <charles.braquet@gmx.fr>
2024-11-24 03:23:29 +0100

    nW switch to openapi

--- pkg/nW/src/index.ts
+++ pkg/nW/src/index.ts
@@ -1,7 +1,7 @@
 import { serve } from '@hono/node-server';
-import { Hono } from 'hono';
-import { z } from 'zod';
-import { zValidator } from '@hono/zod-validator';
+//import { Hono } from 'hono';
+import { z, createRoute, OpenAPIHono } from '@hono/zod-openapi';
+//import { zValidator } from '@hono/zod-validator';
 import esMain from 'es-main';
 
 function addi(aOne: number): number {
@@ -9,17 +9,72 @@ function addi(aOne: number): number {
 	return rNum;
 }
 
-const app = new Hono();
+// openapi routes
+
+const rtGet_root = createRoute({
+	method: 'get',
+	path: '/',
+	request: {},
+	responses: {
+		200: {
+			content: {
+				'text/plain': {
+					schema: z.string()
+				}
+			},
+			description: 'A sweet message'
+		}
+	}
+});
+const rtGet_what = createRoute({
+	method: 'get',
+	path: '/what',
+	request: {
+		query: z.object({ name: z.string() })
+	},
+	responses: {
+		200: {
+			content: {
+				'application/json': {
+					schema: z.object({ ok: z.boolean(), msg: z.string() })
+				}
+			},
+			description: 'Just a question'
+		}
+	}
+});
+const rtGet_addi = createRoute({
+	method: 'get',
+	path: '/addi',
+	request: {
+		query: z.object({ num: z.number({ coerce: true }).int() })
+	},
+	responses: {
+		200: {
+			content: {
+				'application/json': {
+					schema: z.object({ ok: z.boolean(), msg: z.string() })
+				}
+			},
+			description: 'A line of mathematic'
+		}
+	}
+});
+
+// openapi app
+
+const app = new OpenAPIHono();
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const route = app
-	.get('/', (c) => {
+const routeForType = app
+	.openapi(rtGet_root, (c) => {
 		return c.text('Hello Hono!');
 	})
-	.get(
-		'/what',
+	.openapi(
+		//'/what',
+		rtGet_what,
 		//zValidator('query', z.object({ name: z.string().optional() }).optional()),
-		zValidator('query', z.object({ name: z.string() })),
+		//zValidator('query', z.object({ name: z.string() })),
 		(c) => {
 			const query = c.req.valid('query');
 			//console.log(query);
@@ -33,16 +88,30 @@ const route = app
 			);
 		}
 	)
-	.get('/addi', zValidator('query', z.object({ num: z.number({ coerce: true }).int() })), (c) => {
-		const query = c.req.valid('query');
-		return c.json(
-			{
-				ok: true,
-				msg: `5 + ${query.num} = ${addi(query.num)}`
-			},
-			200
-		);
-	});
+	.openapi(
+		//'/addi',
+		rtGet_addi,
+		//zValidator('query', z.object({ num: z.number({ coerce: true }).int() })),
+		(c) => {
+			const query = c.req.valid('query');
+			return c.json(
+				{
+					ok: true,
+					msg: `5 + ${query.num} = ${addi(query.num)}`
+				},
+				200
+			);
+		}
+	);
+
+// The OpenAPI documentation will be available at /doc
+app.doc('/doc', {
+	openapi: '3.0.0',
+	info: {
+		version: '1.0.0',
+		title: 'REST-API of nW'
+	}
+});
 
 if (esMain(import.meta)) {
 	const port = 3010;
@@ -53,5 +122,5 @@ if (esMain(import.meta)) {
 	});
 }
 
-export type nWType = typeof route;
+export type nWType = typeof routeForType;
 export { addi };
