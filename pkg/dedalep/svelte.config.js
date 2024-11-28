7df10b9de7b940d7e60e98642b26554bfbcfa184
charlyoleg <charles.braquet@gmx.fr>
2024-11-28 01:07:31 +0100

    docs on software scalabitiy

--- pkg/dedalep/svelte.config.js
+++ pkg/dedalep/svelte.config.js
@@ -1,15 +1,20 @@
 //import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
 
 const basePath = process.env.BASE_PATH;
 const dev = process.argv.includes('dev') || basePath === undefined;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', '.svx', '.md'],
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({ extensions: ['.svx', '.md'] })
+	],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
