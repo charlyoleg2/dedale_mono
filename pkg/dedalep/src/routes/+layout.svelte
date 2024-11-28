dfe94eee450fcf27be505fc2775138996d4361ba
charlyoleg <charles.braquet@gmx.fr>
2024-11-28 21:59:14 +0100

    sequence diagrams

---
+++ pkg/dedalep/src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	// props
+	interface Props {
+		children?: Snippet;
+	}
+	let { children }: Props = $props();
+</script>
+
+<main>
+	{@render children?.()}
+</main>
+
+<style>
+	:global(body) {
+		font-family: 'Courier New', 'Helvetica', 'Arial', 'Verdana';
+		margin: 0;
+		background-color: LightCyan;
+		color: Indigo;
+	}
+	main {
+		color: Indigo;
+	}
+</style>
