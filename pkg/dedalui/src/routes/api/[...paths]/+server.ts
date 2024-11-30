877bd3d8a598f4b4ea3cdc2f00abc374299066d7
charlyoleg <charles.braquet@gmx.fr>
2024-11-30 22:31:58 +0100

    completed experimentation of api

--- pkg/dedalui/src/routes/api/[...paths]/+server.ts
+++ pkg/dedalui/src/routes/api/[...paths]/+server.ts
@@ -2,5 +2,10 @@ import { apiA } from 'nA';
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = ({ request }) => apiA.fetch(request);
-//export const POST: RequestHandler = ({ request }) => apiA.fetch(request);
+export const POST: RequestHandler = ({ request }) => apiA.fetch(request);
 //export const PUT: RequestHandler = ({ request }) => apiA.fetch(request);
+//export const GET: RequestHandler = async ({ request }) => {
+//	console.log(request);
+//	const lurl = new URL(request.url);
+//	return fetch(`http://localhost:3000${lurl.pathname}${lurl.search}`);
+//};
