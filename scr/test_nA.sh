ba5cfa490da74e3de785ce6f6947d6f0875f37e7
charlyoleg <charles.braquet@gmx.fr>
2024-11-30 21:35:53 +0100

    align on url path

--- scr/test_nA.sh
+++ scr/test_nA.sh
@@ -2,11 +2,11 @@
 # test_nA.sh
 #set -x
 
-curl -w "\n" http://localhost:3000
-curl -w "\n" http://localhost:3000/two?nama="bobo"
-curl -w "\n" http://localhost:3000/addi?numa="20"
-curl -w "\n" http://localhost:3000/search
-curl -w "\n" http://localhost:3000/product?id="paul"
+curl -w "\n" http://localhost:3000/api
+curl -w "\n" http://localhost:3000/api/two?nama="bobo"
+curl -w "\n" http://localhost:3000/api/addi?numa="20"
+curl -w "\n" http://localhost:3000/api/search
+curl -w "\n" http://localhost:3000/api/product?id="paul"
 
 #wget -S -qO- http://localhost:3000/two
 echo -e "\nend of test_nA.sh"
