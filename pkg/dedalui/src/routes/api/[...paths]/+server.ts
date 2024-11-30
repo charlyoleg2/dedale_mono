b7e5a2e3d5525acb2de1fa8c1b47b25ee58118ba
charlyoleg <charles.braquet@gmx.fr>
2024-11-30 21:29:20 +0100

    make api generic

--- pkg/dedalui/src/routes/api/[...paths]/+server.ts
+++ pkg/dedalui/src/routes/api/[...paths]/+server.ts
@@ -3,3 +3,4 @@ import type { RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = ({ request }) => apiA.fetch(request);
 //export const POST: RequestHandler = ({ request }) => apiA.fetch(request);
+//export const PUT: RequestHandler = ({ request }) => apiA.fetch(request);
--- pkg/dedalui/src/routes/api/search/+server.ts
+++
@@ -1,5 +0,0 @@
-import { apiA } from 'nA';
-import type { RequestHandler } from '@sveltejs/kit';
-
-export const GET: RequestHandler = ({ request }) => apiA.fetch(request);
-//export const POST: RequestHandler = ({ request }) => apiA.fetch(request);
