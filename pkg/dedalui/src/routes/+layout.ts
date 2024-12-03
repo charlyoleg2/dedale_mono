75b91e5f63dd497612d9608e32b9d1befdd82b3c
charlyoleg <charles.braquet@gmx.fr>
2024-12-03 22:38:01 +0100

    page options

---
+++ pkg/dedalui/src/routes/+layout.ts
@@ -0,0 +1,5 @@
+// +layout.ts [root]
+
+export const prerender = false; // default: false. if true, generate pages at build time and disable ssr-mode
+export const ssr = true; // default: true. if true, ssr-mode. if false, csr-mode
+export const csr = true; // default: true. if false, remove javascript from page
