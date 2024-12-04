67b44a893a74c80fea1785c0ab0c93dcbc4fbad3
charlyoleg <charles.braquet@gmx.fr>
2024-12-05 00:07:45 +0100

    fix test nZ

--- scr/test_nZ.sh
+++ scr/test_nZ.sh
@@ -2,9 +2,9 @@
 # test_nZ.sh
 #set -x
 
-curl -w "\n" http://localhost:3010/api
-curl -w "\n" http://localhost:3010/api/what?name="bobo"
-curl -w "\n" http://localhost:3010/api/addi?num="20"
+curl -w "\n" http://localhost:3010/apiZ
+curl -w "\n" http://localhost:3010/apiZ/what?name="bobo"
+curl -w "\n" http://localhost:3010/apiZ/addi?num="20"
 
 #wget -S -qO- http://localhost:3010/two
 echo -e "\nend of test_nZ.sh"
