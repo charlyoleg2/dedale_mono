6edfb39500d2cc3c0d8a0c78843f6c9422278ca6
charlyoleg <charles.braquet@gmx.fr>
2024-12-05 20:53:39 +0100

    testing dedalui for addi

--- scr/test_dedalui.sh
+++ scr/test_dedalui.sh
@@ -5,6 +5,7 @@
 curl -w "\n" http://localhost:4173/api/searchAll
 curl -w "\n" http://localhost:4173/api/search?letters="rry"
 curl -w "\n" http://localhost:4173/api/perso?id="paul"
+#curl -w "\n" http://localhost:4173/api/addi?numa="15"
 
 #wget -S -qO- http://localhost:4173/api/searchAll
 echo -e "\nend of test_dedalui.sh"
