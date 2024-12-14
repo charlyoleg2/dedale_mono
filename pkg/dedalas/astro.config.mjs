66dd251a094e0c8e3ff6959f5c6f54efe4bef779
charlyoleg <charles.braquet@gmx.fr>
2024-12-14 17:53:17 +0100

    working on url-base

--- pkg/dedalas/astro.config.mjs
+++ pkg/dedalas/astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+const basePath = process.env.BASE_PATH;
+
 // https://astro.build/config
 export default defineConfig({
-	base: "dedale_mono",
+	base: basePath ? basePath : "/",
 });
