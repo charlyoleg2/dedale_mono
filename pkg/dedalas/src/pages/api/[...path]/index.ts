3b9229b2e9f302bcde448e366b5ed2cd01d3c95d
charlyoleg <charles.braquet@gmx.fr>
2024-12-15 08:22:19 +0100

    rewrite path for hono-api

--- pkg/dedalas/src/pages/api/[...path]/index.ts
+++ pkg/dedalas/src/pages/api/[...path]/index.ts
@@ -7,13 +7,29 @@ import type { APIRoute } from "astro";
 // configuration of integration of Hono
 const honoIntegrated = true; // true: no network call; false: hono must run during the build time
 
-const GETin: APIRoute = async ({ request }) => await apiA.fetch(request);
+// sub-function for writing url
+const base = import.meta.env.BASE_URL;
+const base2 = base === "/" ? "" : base;
+function stripBase(req: Request): Request {
+	const tUrl = new URL(req.url);
+	const tPath = tUrl.pathname;
+	let nPath = tPath;
+	if (base2 !== "") {
+		nPath = tPath.replace(base2, "");
+	}
+	tUrl.pathname = nPath;
+	const rReq = new Request(tUrl, req);
+	return rReq;
+}
+
+const GETin: APIRoute = async ({ request }) =>
+	await apiA.fetch(stripBase(request));
 //const POSTin: APIRoute = async ({ request }) => await apiA.fetch(request);
 //const PUTin: APIRoute = ({ request }) => apiA.fetch(request);
 
 const GETout: RequestHandler = async ({ request }) => {
 	//console.log(request);
-	const lurl = new URL(request.url);
+	const lurl = new URL(stripBase(request.url));
 	const url = `${backCfg.nA_host}:${backCfg.nA_port}${lurl.pathname}${lurl.search}`;
 	//console.log(`dbg295: url: ${url}`);
 	return fetch(url);
